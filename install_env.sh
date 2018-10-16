31ade38ccb76e8ed448b38c79fc967bdd9789da0
Seiji SCHOCH <sesch@smile.fr>
2018-10-16 11:36:50 +0200

    chmod and added a few commands

--- install_env.sh
+++ install_env.sh
@@ -1,7 +1,28 @@
-#Required software
-yum install -y scp
-yum install -y curl
-yum install -y unzip
-yum install -y tar
-yum install -y wget
-yum install -y openssl
+#!bin/bash
+
+yum -y update
+
+#Install utils
+yum -y install scp curl unzip tar wget openssl python27 ntp
+ 
+#Set ulimit to 10000
+ulimit -n 10000;
+
+#Get ambari repo
+wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.1.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+
+#Install ambari server
+yum install -y ambari-server
+
+#Enable NTP (clock)
+systemctl enable ntpd
+
+#iptables config
+systemctl disable firewalld
+service firewalld stop
+
+#Disable SELinux
+setenforce 0
+
+#Set UMASK
+echo umask 0022 >> /etc/profile
