bea51da177e44ffc5b7fe38f7d83bc9c5186e59b
Kuty <quentin.le-baron@epitech.eu>
2018-10-17 21:39:00 +0200

    Add new python install and generate ssh key

--- Install/script/install_env_agent.sh
+++ Install/script/install_env_agent.sh
@@ -2,6 +2,14 @@
 
 yum -y update
 
+# pytho 2.7
+yum install gcc openssl-devel bzip2-devel
+wget https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz
+tar xzf Python-2.7.15.tgz
+cd Python-2.7.15
+./configure --enable-optimizations
+make Install
+
 #Install utils
 yum -y install scp curl unzip tar wget openssl python27 ntp
  
@@ -17,6 +25,9 @@ service firewalld stop
 
 #Disable SELinux
 setenforce 0
+
+# SSH key generation
+ssh-keygen -t rsa -N ""
 
 #Set UMASK
 echo umask 0022 >> /etc/profile
