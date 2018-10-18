e9ab3dedb9d1ab7dca2cc097eb6888c59fbb011c
Seiji SCHOCH <sesch@smile.fr>
2018-10-18 18:08:26 +0200

    Added mysql jdbc jar download

--- Install/script/install_env_server.sh
+++ Install/script/install_env_server.sh
@@ -2,17 +2,23 @@
 
 yum -y update
 
+# pytho 2.7
+yum install gcc openssl-devel bzip2-devel
+wget https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz
+tar xzf Python-2.7.15.tgz
+cd Python-2.7.15
+./configure --enable-optimizations
+make install
+
+# remove install Python
+cd ../
+rm -rf Python-2.7.15
+
 #Install utils
 yum -y install scp curl unzip tar wget openssl python27 ntp
  
 #Set ulimit to 10000
 ulimit -n 10000;
-
-#Get ambari repo
-wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.1.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
-
-#Install ambari server
-yum install -y ambari-server
 
 #Enable NTP (clock)
 systemctl enable ntpd
@@ -24,5 +30,22 @@ service firewalld stop
 #Disable SELinux
 setenforce 0
 
+# SSH key generation
+ssh-keygen -t rsa -N ""
+
 #Set UMASK
 echo umask 0022 >> /etc/profile
+
+#SSH setup
+cat ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+chmod 700 ~/.ssh
+
+chmod 600 ~/.ssh/authorized_keys
+
+#mysql-connector
+sudo yum install mysql-connector-java*
+ls -al /usr/share/java/mysql-connector-java.jar
+cd /var/lib/ambari-server/resources/
+ln -s /usr/share/java/mysql-connector-java.jar mysql-connector-java.jar
+
